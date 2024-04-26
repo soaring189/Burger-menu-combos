b9a782e994aae1d19f21981e481118667ba47e20
soaring189 <127698277+soaring189@users.noreply.github.com>
2024-04-27 10:42:04 +1200

    1

--- Store_existing_combo_meals_&_Output_the_full_menu_v3​.py
+++ Store_existing_combo_meals_&_Output_the_full_menu_v3​.py
@@ -16,13 +16,15 @@ while choice != "Exit" and choice is not None:
             max_name_length = 5
         menu = f"┌{'─' * max_name_length}┬{'─' * max_item_length}┬{'─' * 4}┐\n"
         menu += f"│{' ' * max_name_length * 2}│ Item{' ' * (max_item_length*2-5)}│ Price  │\n"
-        menu += f"├{'─' * max_name_length}┼{'─' * max_item_length}┼{'─' * 4}┤\n"
-        menu += f"│{' ' * int(max_name_length-5)}Combo name{' ' * int(max_name_length-5)}│ Combo items{' ' * (max_item_length * 2 - 12)}│        │\n"
+        menu += f"├{'─' * max_name_length}┼{'─' * max_item_length}┴{'─' * 4}┤\n"
+        menu += f"│{' ' * int(max_name_length-5)}Combo name{' ' * int(max_name_length-5)}│{' ' * (max_item_length - 1)} Combo items{' ' * (max_item_length - 1)}│\n"
         first_outer_loop = True
         for outer_index, (i, j) in enumerate(combos.items()):
             if first_outer_loop:
-                menu += f"├{'─' * max_name_length}┼{'─' * max_item_length}┼{'─' * 4}┤\n"
+                menu += f"├{'─' * max_name_length}┼{'─' * max_item_length}┬{'─' * 4}┤\n"
                 first_outer_loop = False
+            else:
+                menu += f"├{'─' * max_name_length}┼{'─' * max_item_length}┼{'─' * 4}┤\n"
             menu += f"│ {i + ' ' * (max_name_length * 2 - len(i) - 1)}│"
             for inner_index, (k, l) in enumerate(j.items()):
                 menu += f" {k + ' ' * (max_item_length * 2 - len(k) - 1)}│ ${'%.2f' % j[k]}  │\n"
@@ -31,5 +33,4 @@ while choice != "Exit" and choice is not None:
                 if inner_index != len(j) - 1:
                     menu += f"│{' ' * max_name_length * 2}├{'─' * max_item_length}┼{'─' * 4}│\n"
                     menu += f"│{' ' * (max_name_length * 2)}│"
-            first_outer_loop = True
         easygui.msgbox(menu)
